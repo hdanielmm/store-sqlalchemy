742ba2055ed09fa57cfbfa49ec8f4f4493585532
ActiveOne/DanielMarín <daniel.marin@activeone.com>
2022-07-08 18:53:46 -0500

    Add the relationships

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from ctypes import addressof
 from datetime import datetime
 from email.policy import default
 from flask import Flask
@@ -19,14 +18,24 @@ class Customer(db.Model):
   postcode = db.Column(db.String(50), nullable=False)
   email = db.Column(db.String(50), nullable=False, unique=True)
 
+  orders = db.relationship('Order', backref='customer')
+
+order_product = db.Table('order_product',
+  db.Column('order_id', db.Integer, db.ForeignKey('order.id'), primary_key=True),
+  db.Column('product_id', db.Integer, db.ForeignKey('product.id'), primary_key=True)
+)
+
 class Order(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   order_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
   shipped_date = db.Column(db.DateTime)
   delivered_date = db.Column(db.DateTime)
   cupon_code = db.Column(db.String(50))
+  customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+
+  products = db.relationship('Product', secondary='order_product')
 
 class Product(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(50), nullable=False, unique=True)
-  price = db.Column(db.Integer, nullable=False)
+  price = db.Column(db.Integer, nullable=False)  
